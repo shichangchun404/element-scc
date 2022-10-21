e8796b31e055685742a715c545c24a081acb0fac
shizhangchun <shichangchun@jd.com>
2022-10-21 10:43:02 +0800

    update name element-scc

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,16 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-// import '@shicc/theme-chalk/src/index.scss'
-// import SccUI from '@shicc/components'
-// import { SccAlert } from '@shicc/components'
-
+import '@element-scc/theme-chalk/src/index.scss'
+import SccUI from 'element-scc/index'
+// import { SccAlert } from 'element-scc/index'
 // import SccUI from '../../dist/es/index.mjs'
-import { SccAlert } from '../../dist/element-scc/es/index.mjs'
-import '../../dist/element-scc/theme-chalk/css/index.css'
+// import { SccAlert } from '../../dist/element-scc/es/index.mjs'
+// import '../../dist/element-scc/theme-chalk/css/index.css'
 
 const app = createApp(App)
-// app.use(SccUI)
-app.use(SccAlert)
+app.use(SccUI)
+// app.use(SccAlert)
 // app.use(SccMessage)
 app.mount('#app')
